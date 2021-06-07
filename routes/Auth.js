d612242be669a18b8b22e8c262b381436935c2cd
kavi kumaran <kaviathelete@gmail.com>
2021-06-07 12:58:09 +0530

    token error

--- routes/Auth.js
+++ routes/Auth.js
@@ -47,6 +47,6 @@ router.post("/login", async (req, res) => {
   }
 
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-  res.header("auth-token", token).send(token);
+  res.send(token);
 });
 module.exports = router;
